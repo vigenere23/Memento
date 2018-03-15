ea9260a429e551dbd2e1b43e85384df9e4db731f
vigenere23 <lolgab1@hotmail.com>
2018-03-14 22:35:42 -0400

    readded post route for notes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get '/notes', to: 'notes#index'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	get '/notes', to: 'notes#index'
+	post '/notes', to: 'notes#create'
 	root to: 'home#index'
 end
