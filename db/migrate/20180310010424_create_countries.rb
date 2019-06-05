a2d6502d93e46a01fbebcd00455d9f0178483f20
vigenere23 <lolgab1@hotmail.com>
2019-06-05 09:03:03 -0400

    added coutries populator inside migration

--- db/migrate/20180310010424_create_countries.rb
+++ db/migrate/20180310010424_create_countries.rb
@@ -1,8 +1,21 @@
 class CreateCountries < ActiveRecord::Migration[5.1]
   def change
+    require 'csv'
+
     create_table :countries do |t|
       t.string :abbr
       t.string :name
+    end
+
+    csv_text = File.read(
+      Rails.root.join('lib', 'seeds', 'countries.csv')
+    )
+    csv = CSV.parse(csv_text, headers: true)
+    csv.each do |row|
+      country = Country.new
+      country.abbr = row['abbr']
+      country.name = row['name']
+      country.save
     end
   end
 end
